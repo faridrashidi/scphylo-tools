d7d68550f5cbed5f12b1dc2734f3a0a361fb2f3c
Farid Rashidi <farid.rsh@gmail.com>
2021-12-07 11:36:13 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 """Setup Module."""
 
+import os
+import sys
 from pathlib import Path
+from sys import platform
 
 from setuptools import find_packages, setup
+from setuptools.extension import Extension
 
 try:
     from scphylo import __author__, __email__, __maintainer__, __version__
@@ -11,6 +15,108 @@ except ImportError:
     __maintainer__ = ", ".join(["Farid Rashidi"])
     __email__ = ", ".join(["farid.rsh@gmail.com"])
     __version__ = "0.0.0"
+
+if platform == "linux" or platform == "linux2":
+    os.environ["CC"] = "g++"
+elif platform == "darwin":
+    os.environ["CC"] = "clang++"
+extensions = [
+    Extension(
+        "scphylo.external._mltd",
+        sources=["scphylo/external/_mltd.pyx", "scphylo/external/mltd/mltd.cpp"],
+        include_dirs=["scphylo/external/mltd"],
+        extra_compile_args=["-std=c++11"],
+        language="c++",
+    ),
+    Extension(
+        "scphylo.external._scprob",
+        sources=[
+            "scphylo/external/_scprob.pyx",
+            "scphylo/external/scprob/main.cpp",
+        ],
+        include_dirs=["scphylo/external/scprob"],
+        extra_compile_args=["-std=c++11"],
+        language="c++",
+    ),
+    Extension(
+        "scphylo.external._scistree",
+        sources=[
+            "scphylo/external/_scistree.pyx",
+            "scphylo/external/scistree/Utils.cpp",
+            "scphylo/external/scistree/Utils2.cpp",
+            "scphylo/external/scistree/Utils3.cpp",
+            "scphylo/external/scistree/Utils4.cpp",
+            "scphylo/external/scistree/UtilsNumerical.cpp",
+            "scphylo/external/scistree/RerootTreeUtils.cpp",
+            "scphylo/external/scistree/TreeBuilder.cpp",
+            "scphylo/external/scistree/UnWeightedGraph.cpp",
+            "scphylo/external/scistree/MarginalTree.cpp",
+            "scphylo/external/scistree/RBT.cpp",
+            "scphylo/external/scistree/PhylogenyTreeBasic.cpp",
+            "scphylo/external/scistree/PhylogenyTree.cpp",
+            "scphylo/external/scistree/BioSequenceMatrix.cpp",
+            "scphylo/external/scistree/BinaryMatrix.cpp",
+            "scphylo/external/scistree/GenotypeMatrix.cpp",
+            "scphylo/external/scistree/ScistGenotype.cpp",
+            "scphylo/external/scistree/ScistPerfPhyUtils.cpp",
+            "scphylo/external/scistree/ScistPerfPhyImp.cpp",
+            "scphylo/external/scistree/ScistDoublet.cpp",
+            "scphylo/external/scistree/ScistErrRateInf.cpp",
+            "scphylo/external/scistree/main.cpp",
+        ],
+        include_dirs=["scphylo/external/scistree"],
+        extra_compile_args=["-O3", "-std=c++11", "-c"],
+        language="c++",
+    ),
+    Extension(
+        "scphylo.external._scite",
+        sources=[
+            "scphylo/external/_scite.pyx",
+            "scphylo/external/scite/matrices.cpp",
+            "scphylo/external/scite/mcmcBinTreeMove.cpp",
+            "scphylo/external/scite/mcmc.cpp",
+            "scphylo/external/scite/mcmcTreeMove.cpp",
+            "scphylo/external/scite/output.cpp",
+            "scphylo/external/scite/rand.cpp",
+            "scphylo/external/scite/scoreBinTree.cpp",
+            "scphylo/external/scite/scoreTree.cpp",
+            "scphylo/external/scite/treelist.cpp",
+            "scphylo/external/scite/trees.cpp",
+            "scphylo/external/scite/findBestTrees.cpp",
+        ],
+        include_dirs=["scphylo/external/scite"],
+        extra_compile_args=["-O3", "-std=c++11", "-c"],
+        language="c++",
+    ),
+]
+
+
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                sfile = path + ".cpp"
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0)))
+if CYTHONIZE:
+    try:
+        from Cython.Build import cythonize
+    except ImportError:
+        sys.stderr.write(
+            "Cannot find Cython. Have you installed all the requirements?\n"
+            "Try pip install -r requirements.txt\n"
+        )
+        sys.exit(1)
+    compiler_directives = {"language_level": 2, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
 
 if __name__ == "__main__":
     setup(
@@ -25,6 +131,7 @@ if __name__ == "__main__":
         install_requires=[
             r.strip() for r in Path("requirements.txt").read_text("utf-8").splitlines()
         ],
+        ext_modules=extensions,
         extras_require={
             "dev": [
                 "pre-commit",
