2c148609e6e62f91ffe563fa0defb3236dc24e2a
Farid Rashidi <farid.rsh@gmail.com>
2021-12-09 12:32:39 -0500

    add search utils in cli

---
+++ scphylo/commands/utils/_search.py
@@ -0,0 +1,72 @@
+import os
+
+import click
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+import scphylo as scp
+from scphylo.pl._trees import _newick_info2_mutation_list
+
+
+def run_scistree(df_in, alpha, beta, outfile):
+    scp.settings.logfile = f"{outfile}/fn_{beta}-fp_{alpha}.log"
+    df_out = scp.tl.scistree(df_in, alpha, beta)
+    scp.io.write(df_out, f"{outfile}/fn_{beta}-fp_{alpha}.CFMatrix")
+
+    tree = scp.ul.to_tree(df_out)
+    newick, info2, _ = _newick_info2_mutation_list(tree)
+    with open(f"{outfile}/fn_{beta}-fp_{alpha}.newick", "w") as fout:
+        fout.write(newick + "\n")
+    info2.to_csv(f"{outfile}/fn_{beta}-fp_{alpha}.info2", index=None)
+
+
+@click.command(short_help="Grid search for all parameters.")
+@click.argument(
+    "genotype_file",
+    required=True,
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True
+    ),
+)
+@click.option(
+    "--n_threads",
+    "-p",
+    default=-1,
+    type=int,
+    show_default=True,
+    help="Number of threads.",
+)
+def search(genotype_file, n_threads):
+    """Grid search for all parameters of alpha and beta.
+
+    scphylo search input.SC
+    """
+    scp.settings.verbosity = "info"
+
+    outfile = os.path.splitext(genotype_file)[0]
+    scp.ul.mkdir(outfile)
+
+    df_in = scp.io.read(genotype_file)
+
+    betas = [0.1, 0.2, 0.3, 0.4]
+    alphas = [0.1, 0.01, 0.001, 0.0001, 0.00001]
+    n_samples = len(betas) * len(alphas)
+    if n_threads == -1:
+        n_threads = n_samples
+
+    with scp.ul.tqdm_joblib(
+        tqdm(
+            ascii=True,
+            ncols=100,
+            desc="GRID SEARCHING:",
+            total=n_samples,
+            position=0,
+        )
+    ):
+        Parallel(n_jobs=n_threads)(
+            delayed(run_scistree)(df_in, alpha, beta, outfile)
+            for alpha in alphas
+            for beta in betas
+        )
+
+    return None
