0e16cfea4c8a9846d02dd2910616a6b7fcbc1f46
Farid Rashidi <farid.rsh@gmail.com>
2021-12-10 09:41:07 -0500

    add sphylo module

---
+++ scphylo/__init__.py
@@ -0,0 +1,11 @@
+"""scPhylo Module."""
+
+from scphylo import datasets, io
+from scphylo import logging as logg
+from scphylo import pl, pp, settings, tl, ul
+
+__author__ = ", ".join(["Farid Rashidi"])
+__maintainer__ = ", ".join(["Farid Rashidi"])
+__email__ = ", ".join(["farid.rsh@gmail.com"])
+__version__ = "0.0.20"
+__all__ = (datasets, io, logg, pl, pp, settings, tl, ul)
