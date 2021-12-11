1f7b4c848543d133c9a930ec48d85e305b8738ff
Farid Rashidi <farid.rsh@gmail.com>
2021-12-11 00:26:19 -0500

    add settings module

---
+++ scphylo/settings/__init__.py
@@ -0,0 +1,23 @@
+"""Settings Module."""
+
+from scphylo.settings._settings import (
+    hg19,
+    hg38,
+    logfile,
+    m10x,
+    mm10,
+    refs,
+    tools,
+    verbosity,
+)
+
+__all__ = (
+    hg19,
+    hg38,
+    logfile,
+    m10x,
+    mm10,
+    refs,
+    tools,
+    verbosity,
+)
