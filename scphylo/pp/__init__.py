e771d55517aa1d733a9cf7cf3a62c5e8672e2f33
Farid Rashidi <farid.rsh@gmail.com>
2021-12-11 21:12:35 -0500

    add preprocessing module

---
+++ scphylo/pp/__init__.py
@@ -0,0 +1,45 @@
+"""Pre/Post-Processing Module."""
+
+from scphylo.pp._bifiltering import bifiltering
+from scphylo.pp._binary import (
+    binarym_filter_clonal_mutations,
+    binarym_filter_nonsense_mutations,
+    binarym_filter_private_mutations,
+    binarym_statistics,
+    consensus_combine,
+)
+from scphylo.pp._readcount import (
+    build_scmatrix,
+    filter_mut_mutant_must_present_in_at_least,
+    filter_mut_reference_must_present_in_at_least,
+    filter_mut_vaf_greater_than_coverage_mutant_greater_than,
+    filter_snpeff,
+    group_obs_apply_func,
+    keep_cell_by_list,
+    keep_mut_by_list,
+    remove_cell_by_list,
+    remove_mut_by_list,
+    statistics,
+)
+from scphylo.pp._tree import collapse
+
+__all__ = (
+    bifiltering,
+    binarym_filter_clonal_mutations,
+    binarym_filter_nonsense_mutations,
+    binarym_filter_private_mutations,
+    binarym_statistics,
+    consensus_combine,
+    build_scmatrix,
+    filter_mut_mutant_must_present_in_at_least,
+    filter_mut_reference_must_present_in_at_least,
+    filter_mut_vaf_greater_than_coverage_mutant_greater_than,
+    filter_snpeff,
+    keep_cell_by_list,
+    keep_mut_by_list,
+    remove_cell_by_list,
+    remove_mut_by_list,
+    statistics,
+    collapse,
+    group_obs_apply_func,
+)
