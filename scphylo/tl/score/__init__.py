df472f1c44e144eb323775e9e171e58fe51df582
Farid Rashidi <farid.rsh@gmail.com>
2021-12-15 10:10:56 -0500

    add score module

---
+++ scphylo/tl/score/__init__.py
@@ -0,0 +1,6 @@
+"""Score Module."""
+
+from scphylo.tl.score._others import caset, disc, mp3, rf
+from scphylo.tl.score._ours import ad, cc, dl, gs, mltd, tpted
+
+__all__ = (caset, disc, mp3, rf, ad, cc, dl, gs, mltd, tpted)
