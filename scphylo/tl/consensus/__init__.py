d9569c4ab3fac524edcb989d3c3772a4eaabc87c
Farid Rashidi <farid.rsh@gmail.com>
2021-12-15 10:08:23 -0500

    add consensus module

---
+++ scphylo/tl/consensus/__init__.py
@@ -0,0 +1,6 @@
+"""Consensus Module."""
+
+from scphylo.tl.consensus._consensus import consensus
+from scphylo.tl.consensus._consensus_day import consensus_day
+
+__all__ = (consensus, consensus_day)
