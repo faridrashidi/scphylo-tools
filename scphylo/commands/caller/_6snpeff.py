537bc9f07905d28187380a5c417983917cb4d1f2
Farid Rashidi <farid.rsh@gmail.com>
2022-01-30 23:09:54 -0500

    update snpeff

--- scphylo/commands/caller/_6snpeff.py
+++ scphylo/commands/caller/_6snpeff.py
@@ -1,3 +1,4 @@
+import glob
 import subprocess
 
 import click
@@ -9,10 +10,10 @@ from scphylo.ul._servers import cmd, write_cmds_get_main
 
 @click.command(short_help="Run SnpEff.")
 @click.argument(
-    "infile",
+    "outdir",
     required=True,
     type=click.Path(
-        exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True
+        exists=True, file_okay=False, dir_okay=True, readable=True, resolve_path=True
     ),
 )
 @click.argument(
@@ -41,88 +42,67 @@ from scphylo.ul._servers import cmd, write_cmds_get_main
     show_default=True,
     help="Afterok.",
 )
-def snpeff(infile, ref, time, mem, afterok):
+def snpeff(outdir, ref, time, mem, afterok):
     """Run SnpEff.
 
     scphylo snpeff /path/to/vcf/file hg19|hg38|mm10
 
     VCF file --> AnnData file (_genotype.h5ad.gz)
     """
-    outdir, _ = scp.ul.dir_base(infile)
 
-    def step1(afterok):
-        def get_command():
-            cmds = ""
-            cmds += cmd([f"module load {scp.settings.tools['snpeff']}"])
-            cmds += cmd(
-                [
-                    f"java -Xmx{int(mem)-4}g -jar $SNPEFF_JAR",
-                    "-hgvs",
-                    # "-cancer",
-                    # f"-cancerSamples {1}",
-                    ref,
-                    infile,
-                    f"-stats {infile[:-len('.vcf')] + '.snpeff'}"
-                    f"> {infile[:-len('.vcf')] + '.ann.vcf'}",
-                ]
-            )
-            cmds += cmd(["rm -rf", f"{infile[:-len('.vcf')] + '.snpeff.genes.txt'}"])
-            cmds += cmd(
-                [
-                    "mv",
-                    f"{infile[:-len('.vcf')] + '.snpeff'}",
-                    f"{infile[:-len('.vcf')] + '.snpeff.html'}",
-                ]
-            )
-            cmds += cmd(["echo Done!"], islast=True)
-            return cmds
-
-        df_cmds = pd.DataFrame()
-        df_cmds["cmd"] = [get_command()]
-        cmdmain = write_cmds_get_main(
-            df_cmds,
-            "snpeff-1of2",
-            time,
-            mem,
-            None,
-            2,
-            scp.settings.tools["email"],
-            f"{outdir}/_tmp",
-            afterok,
+    def get_command(sample):
+        cmds = ""
+        cmds += cmd([f"module load {scp.settings.tools['snpeff']}"])
+        cmds += cmd(
+            [
+                f"java -Xmx{int(mem)-4}g -jar $SNPEFF_JAR",
+                "-hgvs",
+                # "-cancer",
+                # f"-cancerSamples {1}",
+                ref,
+                f"{outdir}/{sample}.vcf",
+                f"-stats {outdir}/{sample}.snpeff",
+                f"> {outdir}/{sample}.ann.vcf",
+            ]
         )
-        return cmdmain
-
-    def step2(afterok):
-        def get_command():
-            cmds = ""
-            cmds += cmd(
-                [
-                    f"python {scp.ul.get_file('scphylo.ul/cyvcf.py')}",
-                    f"{infile[:-len('.vcf')] + '.ann.vcf'}",
-                ]
-            )
-            cmds += cmd(["echo Done!"], islast=True)
-            return cmds
-
-        cmds_tmp = pd.DataFrame()
-        cmds_tmp["cmd"] = [get_command()]
-        cmdmain = write_cmds_get_main(
-            cmds_tmp,
-            "snpeff-2of2",
-            time,
-            mem,
-            None,
-            2,
-            scp.settings.tools["email"],
-            f"{outdir}/_tmp",
-            afterok,
+        cmds += cmd(["rm -rf", f"{outdir}/{sample}.snpeff.genes.txt"])
+        cmds += cmd(
+            [
+                "mv",
+                f"{outdir}/{sample}.snpeff",
+                f"{outdir}/{sample}.snpeff.html",
+            ]
         )
-        return cmdmain
 
-    cmd1 = step1(afterok)
-    code1 = subprocess.getoutput(cmd1)
-    cmd2 = step2(code1)
-    code2 = subprocess.getoutput(cmd2)
-    scp.logg.info(code2)
+        cmds += cmd(
+            [
+                f"python {scp.ul.get_file('scphylo.ul/cyvcf.py')}",
+                f"{outdir}/{sample}.ann.vcf",
+            ]
+        )
+        cmds += cmd(["echo Done!"], islast=True)
+        return cmds
+
+    files = glob.glob(f"{outdir}/*.vcf")
+    temp = []
+    for file in files:
+        file = file.split("/")[-1].replace(".vcf", "")
+        temp.append({"sample": file})
+    df_cmds = pd.DataFrame(temp)
+    df_cmds["cmd"] = df_cmds.apply(lambda x: get_command(x["sample"]), axis=1)
+
+    cmdmain = write_cmds_get_main(
+        df_cmds,
+        "snpeff",
+        time,
+        mem,
+        None,
+        1,
+        scp.settings.tools["email"],
+        f"{outdir}/_tmp",
+        afterok,
+    )
+    code = subprocess.getoutput(cmdmain)
+    scp.logg.info(code)
 
     return None
