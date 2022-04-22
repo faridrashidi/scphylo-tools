4494328d4f10d43588f410e0e9333e9f8a7a8c62
Farid Rashidi <farid.rsh@gmail.com>
2022-04-22 19:42:18 -0400

    add scite example

---
+++ examples/reconstruction/compute_scite.py
@@ -0,0 +1,28 @@
+"""
+Reconstruct tree by SCITE
+-------------------------
+
+This example shows how to construct a phylogenetic tree using SCITE on a
+binary single-cell genotype matrix.
+"""
+
+import scphylo as scp
+
+# sphinx_gallery_thumbnail_path = "_static/thumbnails/scite.png"
+
+# %%
+# First, we load a binary test single-cell genotype data.
+df_in = scp.datasets.test()
+df_in.head()
+
+# %%
+# Next, using :func:`scphylo.tl.scite` we remove the single-cell noises from the
+# input.
+df_out = scp.tl.scite(df_in, alpha=0.0000001, beta=0.1, n_restarts=3, n_iters=1000)
+df_out.head()
+
+# %%
+# Finally, using :func:`scphylo.ul.is_conflict_free_gusfield` we check whether the
+# inferred genotype matrix is conflict-free or not.
+is_cf = scp.ul.is_conflict_free_gusfield(df_out)
+is_cf
