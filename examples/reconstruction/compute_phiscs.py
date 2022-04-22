bcc94f83cf3556dbfe2862d9f80bf08c94776d8b
Farid Rashidi <farid.rsh@gmail.com>
2022-04-22 19:41:55 -0400

    add phiscs example

---
+++ examples/reconstruction/compute_phiscs.py
@@ -0,0 +1,29 @@
+"""
+Reconstruct tree by PhISCS
+--------------------------
+
+This example shows how to construct a phylogenetic tree using PhISCS on a
+binary single-cell genotype matrix.
+"""
+
+import scphylo as scp
+
+# sphinx_gallery_thumbnail_path = "_static/thumbnails/phiscs.png"
+
+# %%
+# First, we load a binary test single-cell genotype data.
+df_in = scp.datasets.test()
+df_in.head()
+
+# %%
+# Next, using :func:`scphylo.tl.phiscsb` we remove the single-cell noises from the
+# input.
+df_out = scp.tl.phiscsb(df_in, alpha=0.0000001, beta=0.1)
+df_out.head()
+
+
+# %%
+# Finally, using :func:`scphylo.ul.is_conflict_free_gusfield` we check whether the
+# inferred genotype matrix is conflict-free or not.
+is_cf = scp.ul.is_conflict_free_gusfield(df_out)
+is_cf
