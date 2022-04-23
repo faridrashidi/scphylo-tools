4b47775110481915885e0236fbb49c1edb804030
Farid Rashidi <farid.rsh@gmail.com>
2022-04-22 21:24:12 -0400

    add clonal visualization example

---
+++ examples/visualization/plot_clonal.py
@@ -0,0 +1,25 @@
+"""
+Visualizing a tree in clonal format
+-----------------------------------
+
+This example shows how to visualize an inferred tree.
+"""
+
+import scphylo as scp
+
+# sphinx_gallery_thumbnail_path = "_static/thumbnails/clonal.png"
+
+# %%
+# First, we load two binary test single-cell genotype data.
+inferred = scp.io.read(
+    scp.ul.get_file("scphylo.datasets/test/fp_0-fn_0-na_0.ground.CFMatrix")
+)
+
+# %%
+# First we convert the inferred genotype matrix to a tree object.
+tree = scp.ul.to_tree(inferred)
+
+# %%
+# Then we can draw the tree in `clonal` format i.e. mutations at the edges and cells at
+# at the nodes of the tree.
+scp.pl.clonal_tree(tree)
