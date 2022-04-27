2177045b2ec0496bbcee8594f481cbfcfbf5cfe9
Farid Rashidi <farid.rsh@gmail.com>
2022-04-26 22:56:44 -0400

    add sphyr example

---
+++ examples/reconstruction/compute_sphyr.py
@@ -0,0 +1,32 @@
+"""
+Reconstruct tree by SPhyR
+-------------------------
+
+This example shows how to construct a phylogenetic tree using SPhyR on a
+binary single-cell genotype matrix.
+"""
+
+import scphylo as scp
+
+# sphinx_gallery_thumbnail_path = "_static/thumbnails/sphyr.png"
+
+# %%
+# First, we load a binary test single-cell genotype data.
+df_in = scp.datasets.test()
+df_in.head()
+
+# %%
+# Next, using :func:`scphylo.tl.sphyr` we remove the single-cell noises from the
+# input.
+
+# TODO: fix
+# df_out = scp.tl.sphyr(df_in)
+# df_out.head()
+
+
+# %%
+# Finally, using :func:`scphylo.ul.is_conflict_free_gusfield` we check whether the
+# inferred genotype matrix is conflict-free or not.
+
+# is_cf = scp.ul.is_conflict_free_gusfield(df_out)
+# is_cf
