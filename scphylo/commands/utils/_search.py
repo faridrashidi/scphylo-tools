d13f209e9544409b5a04847ce5cafa9a95a15c9b
Farid Rashidi <farid.rsh@gmail.com>
2022-06-20 23:20:17 -0400

    pick the best solution in search

--- scphylo/commands/utils/_search.py
+++ scphylo/commands/utils/_search.py
@@ -1,23 +1,28 @@
 import os
 
 import click
+import numpy as np
 from joblib import Parallel, delayed
 from tqdm import tqdm
 
 import scphylo as scp
-from scphylo.pl._trees import _newick_info2_mutation_list
+
+# from scphylo.pl._trees import _newick_info2_mutation_list
 
 
 def run_scistree(df_in, alpha, beta, outfile):
-    scp.settings.logfile = f"{outfile}/fn_{beta}-fp_{alpha}.log"
+    outfile
+    # scp.settings.logfile = f"{outfile}/fn_{beta}-fp_{alpha}.log"
     df_out = scp.tl.scistree(df_in, alpha, beta)
-    scp.io.write(df_out, f"{outfile}/fn_{beta}-fp_{alpha}.CFMatrix")
+    nll = scp.ul.calc_nll_matrix(df_in, df_out, alpha, beta)
+    # scp.io.write(df_out, f"{outfile}/fn_{beta}-fp_{alpha}.CFMatrix")
 
-    tree = scp.ul.to_tree(df_out)
-    newick, info2, _ = _newick_info2_mutation_list(tree)
-    with open(f"{outfile}/fn_{beta}-fp_{alpha}.newick", "w") as fout:
-        fout.write(newick + "\n")
-    info2.to_csv(f"{outfile}/fn_{beta}-fp_{alpha}.info2", index=None)
+    # tree = scp.ul.to_tree(df_out)
+    # newick, info2, _ = _newick_info2_mutation_list(tree)
+    # with open(f"{outfile}/fn_{beta}-fp_{alpha}.newick", "w") as fout:
+    #     fout.write(newick + "\n")
+    # info2.to_csv(f"{outfile}/fn_{beta}-fp_{alpha}.info2", index=None)
+    return nll, df_out, alpha, beta
 
 
 @click.command(short_help="Grid search for all parameters.")
@@ -44,7 +49,7 @@ def search(genotype_file, n_threads):
     scp.settings.verbosity = "info"
 
     outfile = os.path.splitext(genotype_file)[0]
-    scp.ul.mkdir(outfile)
+    # scp.ul.mkdir(outfile)
 
     df_in = scp.io.read(genotype_file)
 
@@ -63,10 +68,18 @@ def search(genotype_file, n_threads):
             position=0,
         )
     ):
-        Parallel(n_jobs=n_threads)(
+        output = Parallel(n_jobs=n_threads)(
             delayed(run_scistree)(df_in, alpha, beta, outfile)
             for alpha in alphas
             for beta in betas
         )
+        output_i = np.argmin([x[0] for x in output])
+        df_out = output[output_i][1]
+        alpha = output[output_i][2]
+        beta = output[output_i][3]
+        scp.io.write(df_out, f"{outfile}.scistree.CFMatrix")
+        scp.settings.logfile = f"{outfile}.scistree.log"
+        scp.logg.info(f"running ScisTree with alpha={alpha}, beta={beta}, n_threads=1")
+        scp.ul.stat(df_in, df_out, alpha, beta, 0)
 
     return None
