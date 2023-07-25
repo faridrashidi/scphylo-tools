065d519e71d559aae488547b6dca362cba879663
Farid Rashidi <farid.rsh@gmail.com>
2023-07-25 15:57:26 -0400

    fix huntress bug

--- scphylo/tl/solver/huntress/__init__.py
+++ scphylo/tl/solver/huntress/__init__.py
@@ -35,35 +35,29 @@ def huntress(df_input, alpha, beta, n_threads=1):
         f"running HUNTRESS with alpha={alpha}, beta={beta}, n_threads={n_threads}"
     )
 
-    tmpdir = scp.ul.tmpdirsys(suffix=".huntress")
-    df_input_filepath = f"{tmpdir.name}/huntress.SC"
-    scp.io.write(df_input, df_input_filepath)
-
     running_time = 0
     if alpha == 0:
-        running_time = Reconstruct(
-            df_input_filepath,
-            f"{tmpdir.name}/huntress.CFMatrix",
+        matrix_out, running_time = Reconstruct(
+            df_input,
             Algchoice="FN",
             n_proc=n_threads,
         )
-        df_output = pd.read_table(f"{tmpdir.name}/huntress.CFMatrix", index_col=0)
     else:
         fn_conorm = 0.1
         fp_conorm = fn_conorm * alpha / beta
-        running_time = Reconstruct(
-            df_input_filepath,
-            f"{tmpdir.name}/huntress",
+        matrix_out, running_time = Reconstruct(
+            df_input,
             n_proc=n_threads,
             fnfp=51,
             post_fn=fn_conorm,
             post_fp=fp_conorm,
         )
-        df_output = pd.read_table(f"{tmpdir.name}/huntress.CFMatrix", index_col=0)
 
-    df_input = pd.read_table(df_input_filepath, index_col=0)
+    df_output = pd.DataFrame(matrix_out)
+    df_output.columns = df_input.columns
+    df_output.index = df_input.index
+    df_output.index.name = "cellIDxmutID"
+
     scp.ul.stat(df_input, df_output, alpha, beta, running_time)
-
-    tmpdir.cleanup()
 
     return df_output
