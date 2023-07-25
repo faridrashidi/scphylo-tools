9a284f44ec046ca7c03329e88960da042cb2cd31
Farid Rashidi <farid.rsh@gmail.com>
2023-07-25 12:11:40 -0400

    revert back huntress cleanup

--- scphylo/commands/solver/_huntress.py
+++ scphylo/commands/solver/_huntress.py
@@ -34,15 +34,15 @@ import scphylo as scp
 def huntress(genotype_file, alpha, beta, n_threads):
     """HUNTRESS.
 
-    scphylo huntress input.SC 0.0001 0.1 -p 8
+    scphylo solver huntress input.SC 0.0001 0.1 -p 8
     """
+
     outfile = os.path.splitext(genotype_file)[0]
 
     scp.settings.verbosity = "info"
     scp.settings.logfile = f"{outfile}.huntress.log"
 
-    df_in = scp.io.read(genotype_file)
-    df_out = scp.tl.huntress(df_in, alpha=alpha, beta=beta, n_threads=n_threads)
+    df_out = scp.tl.huntress(genotype_file, alpha=alpha, beta=beta, n_threads=n_threads)
     scp.io.write(df_out, f"{outfile}.huntress.CFMatrix")
 
     return None
