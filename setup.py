71e8154c413391aca2d5e07815410fc9a4d47929
Farid Rashidi <farid.rsh@gmail.com>
2025-12-13 18:28:36 -0500

    chore(): 🔧 Update project dependencies and configurations - Upgraded several packages to their latest versions - Adjusted configuration files for compatibility - Removed deprecated dependencies

--- setup.py
+++ setup.py
@@ -5,16 +5,8 @@ import sys
 from pathlib import Path
 from sys import platform
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.extension import Extension
-
-try:
-    from scphylo import __author__, __email__, __maintainer__, __version__
-except ImportError:
-    __author__ = ", ".join(["Farid Rashidi"])
-    __maintainer__ = ", ".join(["Farid Rashidi"])
-    __email__ = ", ".join(["farid.rsh@gmail.com"])
-    __version__ = "0.0.4"
 
 if platform == "linux" or platform == "linux2":
     os.environ["CC"] = "g++"
@@ -115,7 +107,7 @@ if CYTHONIZE:
     if not HAS_CYTHON:
         sys.stderr.write(
             "Cannot find Cython. Have you installed all the requirements?\n"
-            "Try pip install -r requirements.txt\n"
+            "Try pip install Cython\n"
         )
         sys.exit(1)
     compiler_directives = {"language_level": 2, "embedsignature": True}
@@ -126,73 +118,6 @@ else:
 if __name__ == "__main__":
     setup(
         name="scphylo-tools",
-        entry_points="""
-            [console_scripts]
-            scphylo=scphylo.commands.scphylo:cli
-        """,
-        use_scm_version=True,
-        setup_requires=["setuptools_scm"],
-        python_requires=">=3.6",
-        install_requires=[
-            r.strip() for r in Path("requirements.txt").read_text("utf-8").splitlines()
-        ],
         ext_modules=extensions,
-        extras_require={
-            "dev": [
-                "pre-commit",
-                "pytest-cov",
-                "pytest-xdist",
-                "bumpversion",
-            ],
-            "docs": [
-                r.strip()
-                for r in (Path("docs") / "requirements.txt")
-                .read_text("utf-8")
-                .splitlines()
-                if not r.startswith("-r")
-            ],
-        },
-        platforms=["Linux", "MacOSX"],
-        packages=find_packages(),
-        include_package_data=True,
-        author=__author__,
-        author_email=__email__,
-        email=__email__,
-        maintainer=__maintainer__,
-        maintainer_email=__email__,
-        version=__version__,
         description=Path("README.rst").read_text("utf-8").split("\n")[3],
-        long_description=Path("README.rst").read_text("utf-8"),
-        long_description_content_type="text/x-rst; charset=UTF-8",
-        license="BSD",
-        url="https://github.com/faridrashidi/scphylo-tools",
-        project_urls={
-            "Documentation": "https://scphylo-tools.readthedocs.io/en/latest",
-            "Source Code": "https://github.com/faridrashidi/scphylo-tools",
-        },
-        download_url="https://github.com/faridrashidi/scphylo-tools",
-        keywords=[
-            "tumor phylogeny",
-            "single cell",
-            "tools",
-        ],
-        classifiers=[
-            "License :: OSI Approved :: BSD License",
-            "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Developers",
-            "Intended Audience :: Science/Research",
-            "Natural Language :: English",
-            "Framework :: Jupyter",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-            "Operating System :: POSIX :: Linux",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Topic :: Scientific/Engineering :: Bio-Informatics",
-            "Topic :: Scientific/Engineering :: Visualization",
-        ],
     )
