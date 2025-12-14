856c1ba9bb77534e2ee964644cb455a9e592ab47
Farid Rashidi <farid.rsh@gmail.com>
2025-12-14 09:21:16 -0500

    chore(setup): 🔧 Refactor setup.py for Cython integration - Update compiler flags for different platforms - Enhance extension definitions with additional sources - Implement conditional Cythonization based on environment

--- setup.py
+++ setup.py
@@ -1,49 +1,129 @@
-import glob
+"""Setup Module."""
+
 import os
 import sys
+from pathlib import Path
+from sys import platform
 
-from Cython.Build import cythonize
-from setuptools import Extension, setup
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
 
-# Fix for macOS build with PyPy or specific environments
-if sys.platform == "darwin":
-    os.environ["LDFLAGS"] = os.environ.get("LDFLAGS", "").replace(
-        "-Wl,-Bsymbolic-functions", ""
-    )
-
+if platform == "linux" or platform == "linux2":
+    os.environ["CC"] = "g++"
+elif platform == "darwin":
+    os.environ["CC"] = "clang++"
 extensions = [
     Extension(
         "scphylo.external._mltd",
-        ["src/scphylo/external/_mltd.pyx", "src/scphylo/external/mltd/mltd.cpp"],
+        sources=[
+            "src/scphylo/external/_mltd.pyx",
+            "src/scphylo/external/mltd/mltd.cpp",
+        ],
         include_dirs=["src/scphylo/external/mltd"],
-        language="c++",
         extra_compile_args=["-std=c++11"],
-    ),
-    Extension(
-        "scphylo.external._scistree",
-        ["src/scphylo/external/_scistree.pyx"]
-        + glob.glob("src/scphylo/external/scistree/*.cpp"),
-        include_dirs=["src/scphylo/external/scistree"],
         language="c++",
-        extra_compile_args=["-std=c++11"],
-    ),
-    Extension(
-        "scphylo.external._scite",
-        ["src/scphylo/external/_scite.pyx"]
-        + glob.glob("src/scphylo/external/scite/*.cpp"),
-        include_dirs=["src/scphylo/external/scite"],
-        language="c++",
-        extra_compile_args=["-std=c++11"],
     ),
     Extension(
         "scphylo.external._scprob",
-        ["src/scphylo/external/_scprob.pyx", "src/scphylo/external/scprob/main.cpp"],
+        sources=[
+            "src/scphylo/external/_scprob.pyx",
+            "src/scphylo/external/scprob/main.cpp",
+        ],
         include_dirs=["src/scphylo/external/scprob"],
-        language="c++",
         extra_compile_args=["-std=c++11"],
+        language="c++",
+    ),
+    Extension(
+        "scphylo.external._scistree",
+        sources=[
+            "src/scphylo/external/_scistree.pyx",
+            "src/scphylo/external/scistree/Utils.cpp",
+            "src/scphylo/external/scistree/Utils2.cpp",
+            "src/scphylo/external/scistree/Utils3.cpp",
+            "src/scphylo/external/scistree/Utils4.cpp",
+            "src/scphylo/external/scistree/UtilsNumerical.cpp",
+            "src/scphylo/external/scistree/RerootTreeUtils.cpp",
+            "src/scphylo/external/scistree/TreeBuilder.cpp",
+            "src/scphylo/external/scistree/UnWeightedGraph.cpp",
+            "src/scphylo/external/scistree/MarginalTree.cpp",
+            "src/scphylo/external/scistree/RBT.cpp",
+            "src/scphylo/external/scistree/PhylogenyTreeBasic.cpp",
+            "src/scphylo/external/scistree/PhylogenyTree.cpp",
+            "src/scphylo/external/scistree/BioSequenceMatrix.cpp",
+            "src/scphylo/external/scistree/BinaryMatrix.cpp",
+            "src/scphylo/external/scistree/GenotypeMatrix.cpp",
+            "src/scphylo/external/scistree/ScistGenotype.cpp",
+            "src/scphylo/external/scistree/ScistPerfPhyUtils.cpp",
+            "src/scphylo/external/scistree/ScistPerfPhyImp.cpp",
+            "src/scphylo/external/scistree/ScistDoublet.cpp",
+            "src/scphylo/external/scistree/ScistErrRateInf.cpp",
+            "src/scphylo/external/scistree/main.cpp",
+        ],
+        include_dirs=["src/scphylo/external/scistree"],
+        extra_compile_args=["-O3", "-std=c++11", "-c"],
+        language="c++",
+    ),
+    Extension(
+        "scphylo.external._scite",
+        sources=[
+            "src/scphylo/external/_scite.pyx",
+            "src/scphylo/external/scite/matrices.cpp",
+            "src/scphylo/external/scite/mcmcBinTreeMove.cpp",
+            "src/scphylo/external/scite/mcmc.cpp",
+            "src/scphylo/external/scite/mcmcTreeMove.cpp",
+            "src/scphylo/external/scite/output.cpp",
+            "src/scphylo/external/scite/rand.cpp",
+            "src/scphylo/external/scite/scoreBinTree.cpp",
+            "src/scphylo/external/scite/scoreTree.cpp",
+            "src/scphylo/external/scite/treelist.cpp",
+            "src/scphylo/external/scite/trees.cpp",
+            "src/scphylo/external/scite/findBestTrees.cpp",
+        ],
+        include_dirs=["src/scphylo/external/scite"],
+        extra_compile_args=["-O3", "-std=c++11", "-c"],
+        language="c++",
     ),
 ]
 
-setup(
-    ext_modules=cythonize(extensions),
-)
+
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                sfile = path + ".cpp"
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
+
+try:
+    from Cython.Build import cythonize
+
+    HAS_CYTHON = True
+except ImportError:
+    HAS_CYTHON = False
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 1 if HAS_CYTHON else 0)))
+if CYTHONIZE:
+    if not HAS_CYTHON:
+        sys.stderr.write(
+            "Cannot find Cython. Have you installed all the requirements?\n"
+            "Try pip install Cython\n"
+        )
+        sys.exit(1)
+    compiler_directives = {"language_level": 2, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
+
+if __name__ == "__main__":
+    setup(
+        name="scphylo-tools",
+        version="0.0.4",
+        ext_modules=extensions,
+        description=Path("README.rst").read_text("utf-8").split("\n")[3],
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
+    )
