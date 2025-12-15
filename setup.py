e70af0e52462c7ec939486e629bb559f23e8c07d
Farid Rashidi <farid.rsh@gmail.com>
2025-12-14 21:35:16 -0500

    chore(release): 🔧 Bump version 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ if platform == "darwin":
 if __name__ == "__main__":
     setup(
         name="scphylo-tools",
-        version="0.0.4",
+        version="0.0.5",
         ext_modules=extensions,
         description=Path("README.rst").read_text("utf-8").split("\n")[3],
         package_dir={"": "src"},
